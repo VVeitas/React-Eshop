40fc358e1d66580236d9a96607fa4064603d987c
unknown <valentasveitas@yahoo.com>
2020-04-30 09:44:28 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore

--- src/components/form-input/form-input.component.tsx
+++ src/components/form-input/form-input.component.tsx
@@ -17,10 +17,14 @@ const FormInput: React.FC<FormInputProps> = ({
   ...otherProps
 }) => (
   <div className='group'>
-    <input className='form-input' onChange={handleChange} />
+    <input className='form-input' onChange={handleChange} {...otherProps} />
     {label ? (
       <label
-        className={`${otherProps.value.length ? 'shrink' : 'form-input-label'}`}
+        className={`${
+          otherProps.value.length
+            ? 'form-input-label shrink'
+            : 'form-input-label'
+        }`}
       >
         {label}
       </label>
